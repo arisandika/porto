488c2dd97b9ee17a871e6961ca0f8646a7fdb48d
arisandika <ari.bussiness30@gmail.com>
2026-03-17 23:23:25 +0700

    fix pointer and z-index

--- components/layouts/main-layout.tsx
+++ components/layouts/main-layout.tsx
@@ -20,19 +20,13 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       const scrollTop = window.scrollY;
       const windowHeight = window.innerHeight;
       const documentHeight = document.documentElement.scrollHeight;
-
       const scrollThreshold = documentHeight - windowHeight - 200;
 
-      if (scrollTop >= scrollThreshold) {
-        setShowFooter(true);
-      } else {
-        setShowFooter(false);
-      }
+      setShowFooter(scrollTop >= scrollThreshold);
     };
 
     window.addEventListener("resize", handleResize);
     window.addEventListener("scroll", handleScroll);
-
     handleScroll();
 
     return () => {
@@ -46,28 +40,29 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       className="relative flex justify-center w-full min-h-screen overflow-x-hidden"
       style={{ backgroundColor: "var(--background)" }}
     >
-      <div className="absolute inset-0 h-screen overflow-hidden pointer-events-none z-[1]">
+      <div className="absolute inset-0 z-0 h-screen overflow-hidden pointer-events-none">
         <div className="hero-bg-gradient" />
+        <div className="hero-noise" />
       </div>
-
-      <div className="hero-noise" />
 
       <Sidebar isOpen={isMobileMenuOpen} setIsOpen={setIsMobileMenuOpen} />
 
-      <div className="z-10 flex flex-col flex-1 min-h-screen transition-all duration-300">
+      <div className="relative z-10 flex flex-col flex-1 min-h-screen transition-all duration-300">
         <div className="fixed top-0 left-0 right-0 z-50 transition-all duration-300">
           <div className="px-6 py-7 md:px-12">
             <Navbar isOpen={isMobileMenuOpen} setIsOpen={setIsMobileMenuOpen} />
           </div>
         </div>
 
-        <main className="relative z-10 flex justify-center flex-1 w-full section-hero">
-          <div className="w-full px-6 md:px-12 my-30 md:my-40">{children}</div>
-          <div className="hero-bg-gradient"></div>
-          <div className="hero-noise"></div>
+        <main className="relative z-20 flex justify-center flex-1 w-full section-hero">
+          <div className="relative z-30 w-full px-6 md:px-12 my-30 md:my-40">
+            {children}
+          </div>
         </main>
 
-        <Footer />
+        <div className="relative z-20">
+          <Footer />
+        </div>
       </div>
     </div>
   );
